a916d5682a3b638ec434d51c931d835a61b04e66
varnika aluru <aluruvarnika@gmail.com>
2026-09-02 22:09:26 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: '/maps/',
+// https://vite.dev/config/
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/maps/' : '/',
   plugins: [react(), tailwindcss()],
-})
+}))
